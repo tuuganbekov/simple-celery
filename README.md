1381a793773298f5604548c65618296951b90395
tuuganbekov <tuuganbekovb@gmail.com>
2023-04-17 17:05:21 +0600

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # simple-celery
+django-server: python manage.py runserver
+celery worker: celery -A config worker -l INFO
+celery beat: celery -A config beat -l INFO
--- posts/tasks.py
+++ posts/tasks.py
@@ -6,11 +6,14 @@ from posts.models import Post
 
 @shared_task
 def new_post():
+    print("in task")
     post = Post.objects.create(
         title=f"title: {randint(0, 12)}",
         description=f"description: {randint(0, 12)}",
     )
     post.save()
+    print("title:", post.title)
+    return post.title
 
 
 @shared_task
